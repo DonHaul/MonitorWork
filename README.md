fa4062d489449ac918a5d5d5f20d9e7b250a4284
DonHaul <ramiro.animus@gmail.com>
2020-04-24 00:49:49 +0100

    now it can detect the period where the computer/monitorer is offline it can also detect when opening if csv is open somewhere else if some title contains a  ; it converts it to a space more robust overall

--- README.md
+++ README.md
@@ -19,3 +19,4 @@ https://github.com/pypa/setuptools/issues/1963
 Commnad to build
 .\env\Scripts\pyinstaller -y --onefile --noconsole  .\monitorer.py
  .\env\Scripts\pyinstaller -y --onefile  .\monitorer.py  --icon=eye.ico
+ pip freeze > requirements.txt
--- monitorer.py
+++ monitorer.py
@@ -1,7 +1,10 @@
+import logging
+
+
 import  win32gui
 import psutil, win32process
 
-import logging
+
 
 import csv
 import atexit, os
@@ -140,20 +143,16 @@ def DisplayDailyStats(filepath,statevars):
     Displays the category statistics for the day
     '''
 
-    print("entering")
-
     statevars.csvfile.close()
     #Calculate them here:
-    time.sleep(14)
-    df = pd.read_csv(filepath,sep=";",encoding='latin1')
+    df = pd.read_csv(filepath,sep=";",encoding=statevars.encoding)
 
-    statevars.csvfile = open(filepath, 'a')
-    statevars.filewriter = csv.writer(statevars.csvfile, delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
+    statevars.csvfile = open(filepath, 'a',newline='',encoding=statevars.encoding, errors='ignore')
+    statevars.filewriter = csv.writer(statevars.csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+        
 
     #make sure time is in datetime
-    df['Time'] = to_datetime(df['Time'],dayfirst=True)  
-
+    df['Time'] = to_datetime(df['Time'],dayfirst=True)
 
     #set time range for the current day
     start_date= datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
@@ -202,13 +201,15 @@ class MyClass:
   isRunning = True
   csvfile=None
   systray=None
+  encoding="ISO-8859-1"
+  filepath=""
+  errorini=0
 
 def Stop(state):
     state.isRunning=False
 
 def active_window_process_process():
-    pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow()) #This produces a list of PIDs active window relates to
-
+    pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow()) #This produces a list of PIDs active window relates to    
     try:
         return  psutil.Process(pid[-1]).exe() #pid[-1] is the most likely to survive last longer #.name() if you want hust the file not the path
     except:
@@ -216,9 +217,24 @@ def active_window_process_process():
         return "-"
 
 def WriteRowsOnExit(statevars,rows2write):
+
+    #don't try to write to the file if it didnt even initiate capturing
+    if statevars.errorini==1:
+        return
+
+    rows2write.append(["Offline","",datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"),"OFFLINE",""])
+
     while len(rows2write) > 0:
             
         try:
+
+            try:
+                #try open files in case it was closes for some weird reason
+                statevars.csvfile = open(statevars.filepath, 'a',newline='',encoding=statevars.encoding, errors='ignore')
+                statevars.filewriter = csv.writer(statevars.csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+            except:
+                logging.exception("File Open Somewhere Else")
+            
             statevars.filewriter.writerows(rows2write)
             rows2write=[]
             Close(statevars)
@@ -226,9 +242,10 @@ def WriteRowsOnExit(statevars,rows2write):
             
 
         except:
-            logging.warn("Cannot Save at this point, retrying in 60 seconds")
+            logging.exception("Cannot Save at this point, retrying in 60 seconds")
             time.sleep(60)
 
+            
             try:
                 toaster = ToastNotifier()
                 toaster.show_toast("Work Monitoring",    "Cannot Save File! Please close the excel file bro",    icon_path="eye.ico",    duration=5,    threaded=False)
@@ -246,13 +263,13 @@ def Monitor(statevars,interval,classesfile='info.json',directory="Outputs/"):
 
 
     #sets path for file
-    filepath = directory + str(datetime.date.today().strftime("%m-%Y")) + ".csv"
+    statevars.filepath = directory + str(datetime.date.today().strftime("%m-%Y")) + ".csv"
     
     #filds on top of the excel document
     fields=None
 
     #createæxcel if it does exist else just load it 
-    if not os.path.isfile(filepath):  # False
+    if not os.path.isfile(statevars.filepath):  # False
         
         #addfields later
         fields = ['Category','WindowName','Time','MouseState','Path']
@@ -266,7 +283,7 @@ def Monitor(statevars,interval,classesfile='info.json',directory="Outputs/"):
 
             import shutil
 
-            shutil.copyfile(filepath, filepath+str(time.time))
+            shutil.copyfile(statevars.filepath, statevars.filepath+str(time.time))
         
             
             
@@ -282,7 +299,7 @@ def Monitor(statevars,interval,classesfile='info.json',directory="Outputs/"):
     #Set Up System Tray Icon
 
     #Show stats button
-    menu_options = (("Show Stats", None, lambda s :DisplayDailyStats(filepath,statevars)),)
+    menu_options = (("Show Stats", None, lambda s :DisplayDailyStats(statevars.filepath,statevars)),)
     #choose icon and how to quit
     statevars.systray = SysTrayIcon("eye.ico", "Monitorer", menu_options, on_quit=lambda s :Stop(statevars))
     statevars.systray.start()
@@ -293,8 +310,21 @@ def Monitor(statevars,interval,classesfile='info.json',directory="Outputs/"):
     #initialize activity classifier
     mouseclassifier = IdleClassifier()
 
-    statevars.csvfile = open(filepath, 'a',newline='',encoding="latin-1")
-    statevars.filewriter = csv.writer(statevars.csvfile, delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    #if csv file is open then ask to close it and exit
+    try:
+        statevars.csvfile = open(statevars.filepath, 'a',newline='',encoding=statevars.encoding, errors='ignore')
+        statevars.filewriter = csv.writer(statevars.csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+    except:
+        try:
+            toaster = ToastNotifier()
+            toaster.show_toast("Work Monitoring",    "Close The CSV file before starting the monitoring",    icon_path="eye.ico",    duration=5,    threaded=False)
+        except:
+            logging.warning("Issue displaying desktop notification")
+
+        #exit with error initiating
+        statevars.errorini = 1
+
+        sys.exit()
 
     #open file read and write
     #https://stackoverflow.com/questions/14978575/writing-reading-the-same-csv-file-in-python
@@ -302,7 +332,7 @@ def Monitor(statevars,interval,classesfile='info.json',directory="Outputs/"):
     
     
     if(fields != None):
-        filewriter.writerow(fields)
+        statevars.filewriter.writerow(fields)
 
         #open listener to read incoming inputs
     with Listener(on_press=mouseclassifier.UpdateKeyTime) as listener:
@@ -312,6 +342,7 @@ def Monitor(statevars,interval,classesfile='info.json',directory="Outputs/"):
 
             #window name
             string = GetWindowName()
+            string = string.replace(";","-")
 
             #date
             dt_string = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -328,13 +359,13 @@ def Monitor(statevars,interval,classesfile='info.json',directory="Outputs/"):
             output = [ curclass, string,dt_string,userstate,path]
 
             rows2write.append(output)
-            #print(filepath,output)
+            #print(statevars.filepath,output)
             #write in row
             try:
                 statevars.filewriter.writerows(rows2write)
                 rows2write=[]
             except:
-                logging.warning("Could not write at this time, writing later")
+                logging.exception("Could not write at this time, writing later")
 
             
             #make trigger with focus later
@@ -352,7 +383,9 @@ def Monitor(statevars,interval,classesfile='info.json',directory="Outputs/"):
 
             
 def Close(statevars):
-    statevars.csvfile.close()
+
+    if statevars.csvfile is not None:
+        statevars.csvfile.close()
     statevars.systray.shutdown()
 
 def main():
